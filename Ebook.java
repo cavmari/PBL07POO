48002a58dd82e976316f0b791c104caa7b737b01
mari <117183854+cavmari@users.noreply.github.com>
2024-04-19 23:01:59 -0300

    Ebook.java

---
+++ Ebook.java
@@ -0,0 +1,19 @@
+package library.items;
+
+import library.interfaces.Baixavel;
+
+public class Ebook extends ItemBibliotecaDigital implements Baixavel {
+    public Ebook(String titulo, String autor) {
+        super(titulo, autor);
+    }
+
+    @Override
+    public String descricao() {
+        return "Ebook: " + titulo + " - Autor: " + autor;
+    }
+
+    @Override
+    public void baixar() {
+        System.out.println("Baixando o ebook: " + titulo);
+    }
+}
